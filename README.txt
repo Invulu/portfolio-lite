3c7f71594270bf73e053f403390f18c6999db50c
David Morgan <itsdavidmorgan@gmail.com>
2017-10-19 09:57:54 -1000

    Updates to admin notice

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 === Portfolio Lite ===
 
-Version: 1.1.5
+Version: 1.1.6
 Contributors: organicthemes, itsdavidmorgan
 Tags: portfolio, photography, blog, one-column, two-columns, right-sidebar, custom-background, custom-header, custom-menu, custom-logo, featured-images, featured-image-header, flexible-header, full-width-template, translation-ready, sticky-post, threaded-comments, editor-style, theme-options
 Requires at least: 4.7
@@ -68,6 +68,9 @@ GNU General Public License for more details.
 * jquery.flexslider.js, Licensed under GPLv2 or later.
 
 == Changelog ==
+
+== 1.1.6 ==
+* Updates to admin notice
 
 == 1.1.5 ==
 * Regenerated .pot language file
--- functions.php
+++ functions.php
@@ -271,38 +271,64 @@ function portfolio_lite_admin_notice() {
 		return;
 	}
 	?>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=246727095428680";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<script>window.twttr = (function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0],
+		t = window.twttr || {};
+		if (d.getElementById(id)) return t;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "https://platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js, fjs);
+
+		t._e = [];
+		t.ready = function(f) {
+			t._e.push(f);
+		};
+
+		return t;
+	}(document, "script", "twitter-wjs"));</script>
+
 	<div data-dismissible="notice-portfolio-lite-30" class="notice updated is-dismissible">
-		<p><?php printf( __( 'Enjoying the <b>Portfolio Lite</b> theme? Enter your email to receive updates and information from <a href="%1$s" target="_blank">Organic Themes</a>.', 'portfolio-lite' ), 'https://organicthemes.com/themes/' ); ?></p>
-		<div id="mc_embed_signup" class="clear" style="overflow: hidden; margin-bottom: 12px;">
-			<form action="//organicthemes.us1.list-manage.com/subscribe/post?u=7cf6b005868eab70f031dc806&amp;id=c3cce2fac0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<div id="mc_embed_signup_scroll">
-					<div id="mce-responses" class="clear">
-						<div class="response" id="mce-error-response" style="display:none"></div>
-						<div class="response" id="mce-success-response" style="display:none"></div>
+
+		<p><?php printf( __( 'Enjoying the <b>Portfolio Lite</b> theme? Enter your email to receive updates and information from <a href="%1$s" target="_blank">Organic Themes</a>. For instructions please refer to our <a href="%2$s" target="_blank">theme setup guide</a>.', 'portfolio-lite' ), 'https://organicthemes.com/themes/', 'https://organicthemes.com/the-free-wordpress-portfolio-lite-theme/' ); ?></p>
+
+		<div class="follows" style="overflow: hidden; margin-bottom: 12px;">
+
+			<div id="mc_embed_signup" class="clear" style="float: left;">
+				<form action="//organicthemes.us1.list-manage.com/subscribe/post?u=7cf6b005868eab70f031dc806&amp;id=c3cce2fac0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>
+						<div class="mc-field-group" style="float: left;">
+							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+						</div>
+						<div style="float: left; margin-left: 6px;"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7cf6b005868eab70f031dc806_c3cce2fac0" tabindex="-1" value=""></div>
 					</div>
-					<div class="mc-field-group" style="float: left;">
-						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
-					</div>
-					<div style="float: left; margin-left: 6px;"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7cf6b005868eab70f031dc806_c3cce2fac0" tabindex="-1" value=""></div>
-				</div>
-			</form>
+				</form>
+			</div>
+
+			<div class="social-links" style="float: left; margin-left: 24px; margin-top: 4px;">
+				<div class="fb-like" style="float: left;" data-href="https://www.facebook.com/OrganicThemes/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+				<div class="twitter-follow" style="float: left; margin-left: 6px;"><a class="twitter-follow-button" href="https://twitter.com/OrganicThemes" data-show-count="false">Follow @OrganicThemes</a></div>
+			</div>
+
 		</div>
+
 	</div>
-	<style>
-		#mce-responses .response {
-			margin-bottom: 12px;
-			padding: 6px 12px;
-			background-color: #ccffcc;
-			border-radius: 3px;
-		}
-		#mce-responses #mce-error-response {
-			background-color: #ffcccc;
-		}
-		div.mce_inline_error {
-			display: none !important;
-		}
-	</style>
+
 	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 	<!--End mc_embed_signup-->
 	<?php
